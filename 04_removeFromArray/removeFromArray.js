2635ba887e67b9f7548a13ff7a5968bbf29bab7f
Manish <msn07082005@gmail.com>
2023-12-30 19:28:34 +0530

    Completed Question 4!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr,...num) {
+    let size=arr.length;
+    for(target of num)
+        for(let i=0;i<size;++i)
+            if(arr[i]===target){
+                arr.splice(i,1);
+                size--;
+                i--;
+            }
+    return arr;
 };
 
 // Do not edit below this line
