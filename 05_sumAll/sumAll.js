053fa213457ff7a34b5d0c735d9ffbd1184720a8
Manish <msn07082005@gmail.com>
2023-12-30 19:40:56 +0530

    Completed Question 5!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,14 @@
-const sumAll = function() {
+const sumAll = function(left,right) {
+    let ans=0;
+    if(left<0 || right<0 || !Number.isInteger(left) || !Number.isInteger(right))
+        return "ERROR";
+    if(left>right)
+        return sumAll(right,left);
+    while(left<=right){
+        ans+=left;
+        left++;
+    }
+    return ans;
 
 };
 
