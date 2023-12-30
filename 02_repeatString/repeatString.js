605052da806f7ee12006e3181da492d02e370cbe
Manish <msn07082005@gmail.com>
2023-12-30 19:07:17 +0530

    Completed Question 2!

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(word,times) {
+    let ans='';
+    if(times<0)
+        return 'ERROR';
+    while(times--){
+        ans+=`${word}`;
+    }
+    return ans;
 };
 
 // Do not edit below this line
