24d16a950306b5bc4353a509d4edab02ea756085
Manish <msn07082005@gmail.com>
2023-12-30 19:17:06 +0530

    Completed Question 3!

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = word=> {
+    const characters=word.split("");
+    const reverses=[];
+    for(let i=0;i<characters.length;++i)
+        reverses[i]=characters[characters.length-i-1];
+    return reverses.join('');
 };
 
 // Do not edit below this line
