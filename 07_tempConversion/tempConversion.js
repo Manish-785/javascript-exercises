0fbf4116b00e8cc6544ca899ff659e748f7e33d4
Manish <msn07082005@gmail.com>
2023-12-30 19:53:48 +0530

    Question 7 Completedgit add tempConversion.spec.js tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  return parseFloat((Math.round((temp-32)*50/9)/10).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return parseFloat((Math.round(18*temp+320)/10).toFixed(1));
 };
 
 // Do not edit below this line
